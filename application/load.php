25a7bec4717816782b53fc329b4c45cba5845d3b
John Hagler <haglerjo@gmail.com>
2013-02-04 21:22:31 -0800

    added method to populate object with request data

--- application/load.php
+++ application/load.php
@@ -8,6 +8,23 @@ class Load {
 		include 'views/' . $file_name;
 		include 'views/footer.php';
 	}
+
+	function populate($obj) {
+
+		$reflect = new ReflectionClass($obj);
+
+		$props   = $reflect->getProperties(ReflectionProperty::IS_PUBLIC | ReflectionProperty::IS_PROTECTED);
+
+		foreach ($props as $prop) {
+			$name = $prop->getName();
+			$value = $_REQUEST[$name];
+			$obj->{$name} = $value;
+		}
+
+		
+
+	}
+
 }
 
 ?>
