914ecd9d5a762bfe0c1bcfe71f57df11c803d118
John Hagler <haglerjo@gmail.com>
2013-02-18 18:53:50 -0800

    mysql database setup script

---
+++ setupMySQL/create_table_user.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `User` (
+  `UserID` varchar(50) NOT NULL,
+  `FirstName` varchar(50) NOT NULL,
+  `LastName` varchar(50) NOT NULL,
+  `Password` varchar(50) NOT NULL,
+  PRIMARY KEY (`UserID`)
+) ; 
