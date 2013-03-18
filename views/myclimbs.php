04e4d59e09d7c44032f5d40d28290e8631d30476
John Hagler <haglerjo@gmail.com>
2013-03-17 18:07:32 -0700

    prep for listing and interacting with attempts

--- views/myclimbs.php
+++ views/myclimbs.php
@@ -4,7 +4,11 @@
 		<h3><small>Look at all that climbing you did!  You ROCK!</small></h3>
 	</div>
 </div>
-
+<style>
+a.remove:hover {
+	color:#c60f13;
+}
+</style>
 
 <div class="row">
 	<div class="twelve columns">
@@ -14,11 +18,8 @@
 				<th><a href="#">Crag</a></th>
 				<th><a href="#">Route</a></th>
 				<th><a href="#">Difficulty</a></th>
-				<th><a href="#">Attempts</a></th>
-				<th><a href="#">Summits</a></th>
-				<th><a href="#">Fun factor</a></th>
 				<th><a href="#">Rating</a></th>
-				<th><a href="#">Last Attempt</a></th>
+				<th></th>
 			</tr>
 			<tr>
 				<td></td>
@@ -26,10 +27,19 @@
 				<td></td>
 				<td></td>
 				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
+				<td style="padding:0;" >
+					<ul class="button-group radius" style="margin:0;">
+						<li><a href="" class="button tiny secondary">
+							<i class="foundicon-flag"></i>
+						</a></li>
+						<li><a href="" class="button tiny alert">
+							<i class="foundicon-error"></i>
+						</a></li>
+						<li><a href="" class="button tiny secondary remove">
+							<i class="foundicon-remove"></i>
+						</a></li>
+					</ul>
+				</td>
 			</tr>
 			<tr>
 				<td></td>
@@ -37,51 +47,69 @@
 				<td></td>
 				<td></td>
 				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-			</tr><tr>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-			</tr><tr>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-			</tr><tr>
+				<td style="padding:0;" >
+					<ul class="button-group radius" style="margin:0;">
+						<li><a href="" class="button tiny secondary">
+							<i class="foundicon-flag"></i>
+						</a></li>
+						<li><a href="" class="button tiny alert">
+							<i class="foundicon-error"></i>
+						</a></li>
+						<li><a href="" class="button tiny secondary remove">
+							<i class="foundicon-remove"></i>
+						</a></li>
+					</ul>
+				</td>
+			</tr>
+			<tr>
 				<td></td>
 				<td></td>
 				<td></td>
 				<td></td>
 				<td></td>
+				<td style="padding:0;" >
+					<ul class="button-group radius" style="margin:0;">
+						<li><a href="" class="button tiny secondary">
+							<i class="foundicon-flag"></i>
+						</a></li>
+						<li><a href="" class="button tiny alert">
+							<i class="foundicon-error"></i>
+						</a></li>
+						<li><a href="" class="button tiny secondary remove">
+							<i class="foundicon-remove"></i>
+						</a></li>
+					</ul>
+				</td>
+			</tr>
+			<tr>
 				<td></td>
 				<td></td>
 				<td></td>
 				<td></td>
-			</tr><tr>
 				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
+				<td style="padding:0;" >
+					<ul class="button-group radius" style="margin:0;">
+						<li><a href="" class="button tiny secondary">
+							<i class="foundicon-flag"></i>
+						</a></li>
+						<li><a href="" class="button tiny alert">
+							<i class="foundicon-error"></i>
+						</a></li>
+						<li><a href="" class="button tiny secondary remove">
+							<i class="foundicon-remove"></i>
+						</a></li>
+					</ul>
+				</td>
 			</tr>
 		</table>
 	</div>
 </div>
+
+
+<script>
+	$('a.remove').click(function(){
+		$(this).parents('tr').remove();
+		return false;
+	});
+
+</script>
