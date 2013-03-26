18c8e08ce89f74a88b573634474be2dd4a0f07f1
Anna <achernyccsu@gmail.com>
2013-03-25 22:12:34 -0400

    about us

--- views/about.php
+++ views/about.php
@@ -9,7 +9,20 @@
 <div class="row">
 	<div class="twelve columns">
 		<h2>What we're about...</h2>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque numquam iste culpa voluptates veniam enim illo earum ipsum facilis quod pariatur natus tenetur officia mollitia amet vero doloremque officiis cum!</p>
+		<p>Hi there and welcome to ClimbIt!</p>
+		<p>Are you a climbing enthusiast who enjoys spending more time on the crag working on new climbs than spending many hours online 
+			looking for new routes that fit your grade and ability level? Then this site is for you. We know how important it is to be 
+			able to plan your climbs and projects to work on and track your progress in order to make your climbing experiences most 
+			memorable and fulfilling. So we made it our goal to provide a user-friendly, centralized place for fellow climbers to search 
+			for routes and track their climbing experiences in the most convenient and efficient way. We do so by offering a comprehensive, 
+			regularly updated online database on climbing areas, crags and routes across the United States. In addition, our site provides registered 
+			users with the ability to log their climbing attempts, view logged climbs and track their progress overtime. </p>
+		<p>As we strive to offer the best experience for our users, we would love to hear from you! Can't find an area, crag or route that 
+			you would like to have listed? Have suggestions on the site use? Want to share a story about your recent climbing experience? We 
+			are here to help you find your next climbing project and track your progress in the most effortless and fun way. So drop us an 
+			e-mail and we'll be delighted to respond to any questions, recommendations to the site, suggestion on updates to the database…or 
+			we are just as happy to simply hear your thoughts.</p>
+		<p>In the meantime, go FindIt and SendIt!</p>
 	</div>
 </div>
 
@@ -21,11 +34,14 @@
 
 		<div class="row">
 			<div class="six columns">
-				<img src="http://placehold.it/125x125" alt="Anna">
+				<img src="images/anna.jpg" alt="Anna">
 				<h3>Anna</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus sit velit laborum facilis? Explicabo eum libero quis deleniti fuga dolor est id veniam eius architecto asperiores itaque! Aliquid hic dolor!
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque hic ipsa minima obcaecati est culpa debitis veniam nemo id non nostrum optio perspiciatis sequi ullam earum dignissimos quaerat. Quas molestiae.
+				<p>I first tried climbing in early 2012, fell in love with the sport after my first climb and quickly
+					got my entire family into climbing as well. Since then climbing has become not just my passion, but a favorite family 
+					activity for me, my husband and our two daughters, Elizabeth and Sofia, whose excitement about the sport and fearless 
+					spirit motivate, encourage and constantly push me to the new limits. On a professional side, I am a financial 
+					systems consultant by day. When I am not crunching numbers or climbing, I enjoy traveling, exploring new places and 
+					experiencing new things with my family. Climb on and have fun!
 				</p>
 
 			</div>
